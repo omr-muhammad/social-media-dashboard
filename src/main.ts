db9a3d5923db659bee763decec456925b3f31acc
unknown <omarelsayedmuhammad@gmail.com>
2024-06-22 23:32:20 +0300

    finished header section and theme switcher functionality

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import "./style.scss";
+
+const themeToggler = document.querySelector(
+  "[data-change-theme]"
+) as HTMLDivElement;
+
+themeToggler.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark");
+  document.querySelector("[data-switch]")?.classList.toggle("toggle");
+});
